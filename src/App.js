8eabf7032d34288e0d841a321150dc0fed33e15c
peninsula101 <tvscskiriti@gmail.com>
2024-09-20 06:25:45 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Counter from './components/Counter'
+/*import Welcome from './components/Welcome'
+
+const App = () => {
+  return <Welcome name="Krishna" />
+}*/
+
+const App = () => {
+  return <Counter />
+}
+
+export default App
